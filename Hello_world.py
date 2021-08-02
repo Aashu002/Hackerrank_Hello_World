8f923f9362356d21793e042a7da374ab611bd4ac
ASHUTOSH SAHU <88073457+Aashu002@users.noreply.github.com>
2021-08-02 20:39:39 +0530

    Create Hello_world.py

---
+++ Hello_world.py
@@ -0,0 +1,9 @@
+'''
+Title     : Say Hello, World! With Python
+Subdomain : Introduction
+Domain    : Python
+Author    : Ahmedur Rahman Shovon
+Created   : 23 July 2018
+Problem   : https://www.hackerrank.com/challenges/py-hello-world/problem
+'''
+print("Hello, World!") #Write Hello, World as it is mentioned in statement otherwise it will throw error
